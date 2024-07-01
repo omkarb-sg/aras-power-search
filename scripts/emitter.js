975b8dbb09196605b565387f9e3ea62961ee9067
tushark <tushark@steepgraph.com>
2024-07-01 13:59:08 +0530

    (feat) Add emitter

--- scripts/emitter.js
+++ scripts/emitter.js
@@ -22,7 +22,7 @@ console.log("Watching...")
 fs.watch(filePath, (eventType, filename) => {
   console.log("Some file changed");
   if (filename && eventType === 'change') {
-    
+
     fs.readFile(filePath, 'utf8', (err, data) => {
       if (err) {
         console.error('Error reading file:', err);
@@ -47,4 +47,37 @@ io.on('connection', (socket) => {
 server.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
+
+`
+// ==UserScript==
+// @name        aras-power-search emitter
+// @namespace   aras-power-tools
+// @match       *://*/*/Client/*
+// @grant       none
+// @version     1.0
+// @author      Tushar KUNTAWAR
+// @require     https://cdn.socket.io/4.0.0/socket.io.min.js
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  if (window !== window.top || !window.top.aras) {
+    return
+  }
+  const socket = io('http://localhost:3000');
+
+  eval(localStorage.getItem("_aras_power_search_code") || \`top.aras.getNotifyByContext(window)('Nothing in LS', { type: 'error' })\`) ;
+    socket.on('file-changed', (data) => {
+      aras.AlertSuccess("Reloading");
+      localStorage.setItem("_aras_power_search_code", data);
+      window.location.reload();
+    });
+
+    console.log('Violentmonkey script running and listening for file changes...');
+})();
+`
+
+
+
+
 
