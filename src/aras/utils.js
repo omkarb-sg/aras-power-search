df1f254319a68ae5ac102d235b15de68f81e6617
tushark <tushark@steepgraph.com>
2024-11-16 09:29:39 +0530

    rewrite

--- src/aras/utils.js
+++
@@ -1,7 +0,0 @@
-function getUrlFromFileId(aras, fileId) {
-    let file = aras.IomInnovator.newItem("File", "get");
-    file.setAttribute("id", fileId);
-    file = file.apply();
-    if (file.isError()) return null;
-    return aras.vault.vault.makeFileDownloadUrl(aras.getFileURLEx(file.node));
-}
